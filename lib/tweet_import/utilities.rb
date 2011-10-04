f0ca007329df78f4e4f2fab61cc0810b4e72c1f7
Jonathan Julian <jonathan.m.julian@gmail.com>
2011-10-04 17:04:54 -0400

    a broader regex

--- lib/tweet_import/utilities.rb
+++ lib/tweet_import/utilities.rb
@@ -3,7 +3,7 @@ module TweetImport
 
     def extract_tweet_id(str)
       return str.to_s unless str.respond_to?(:match)
-      m = str.match /https:\/\/twitter.com\/#!\/\w+\/status\/(\d+)/
+      m = str.match /twitter.com\/(?:#!\/)?\w+\/status\/(\d+)/i
       m ? m[1] : str.to_s
     end
 
--- test/utilities_test.rb
+++ test/utilities_test.rb
@@ -6,6 +6,12 @@ class UtilitiesTest < Test::Unit::TestCase
   def test_tweet_id_is_pulled_from_a_url
     assert_equal '118745990760103936', extract_tweet_id('https://twitter.com/#!/jonathanjulian/status/118745990760103936')
   end
+  def test_tweet_id_is_pulled_from_an_old_url
+    assert_equal '118745990760103936', extract_tweet_id('https://twitter.com/jonathanjulian/status/118745990760103936')
+  end
+  def test_tweet_id_is_pulled_from_a_no_scheme_url
+    assert_equal '118745990760103936', extract_tweet_id('twitter.com/jonathanjulian/status/118745990760103936')
+  end
   def test_tweet_id_is_returned_untouched
     assert_equal '118745990760103936', extract_tweet_id('118745990760103936')
   end
